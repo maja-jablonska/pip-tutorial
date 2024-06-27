6bd7896e312ccd30fc909a9340a881229b15fb03
Maja Jablonska <majajjablonska@gmail.com>
2024-06-27 14:50:35 +1000

    remove build

--- build/lib/gasp_pip_tutorial/__init__.py
+++
@@ -1,1 +0,0 @@
-from .meow import simple_meow, meow_with_intent
--- build/lib/gasp_pip_tutorial/meow.py
+++
@@ -1,6 +0,0 @@
-def simple_meow() -> None:
-    print("Meow!")
-    
-
-def meow_with_intent(intent: str) -> None:
-    print(f"Meow, {intent}!")
